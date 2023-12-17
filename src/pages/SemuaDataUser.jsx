7d47cbf268a81906e9fd0f3ccb97a61641076184
Frans701 <franswahyu7@gmail.com>
2023-12-17 21:20:42 +0800

    first commit

--- src/pages/SemuaDataUser.jsx
+++ src/pages/SemuaDataUser.jsx
@@ -33,20 +33,7 @@ function SemuaDataUSer() {
     <>
       <div className="px-[40px] text-xs flex justify-center flex-col items-center gap-[60px]">
         <div className="py-16 px-8 shadow-lg rounded-xl flex flex-col justify-center items-center gap-10">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-20 h-20"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-            />
-          </svg>
+          <h1 className="text-xl font-bold">Selamat Datang</h1>
           {loading ? (
             <div className="w-60 flex flex-row justify-center">
               <PuffLoader color="#fff" />
